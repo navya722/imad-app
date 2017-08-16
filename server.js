20617adf3a8fa4acee580f76870fe6a5682d0cd6
navya722 <30749696+navya722@users.noreply.github.com>
2017-08-16 11:08:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,15 +14,15 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one', function(req,res){
-   res.send('Article one reuested and will be served here') 
+   res.send('Article one reuested and will be served here');
 });
 
 app.get('/article-two', function(req,res){
-   res.send('Article two reuested and will be served here') 
+   res.send('Article two reuested and will be served here'); 
 });
 
 app.get('/article-three', function(req,res){
-   res.send('Article three reuested and will be served here') 
+   res.send('Article three reuested and will be served here');
 });
 
 app.get('/ui/madi.png', function (req, res) {
