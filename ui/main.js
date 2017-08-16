b39c3cba31bf2142ead762806397710f5d65e887
navya722 <30749696+navya722@users.noreply.github.com>
2017-08-16 15:01:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,17 @@
 console.log('Loaded!');
 var element= document.getElementById("main-text");
 element.innerHTML = 'New value';
+
 //Move the image
 var img= document.getElementById('madi');
-img.onclick= function(){
-  img.style.marginLeft='100px';  
+var marginLeft=0;
+
+function moveRight() {
+    marginLeft= marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
+
+img.onclick= function() {
+    var interval= setInterval(moveRight,100)
+ // img.style.marginLeft='100px';  
 };
